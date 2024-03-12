e97bb0b56b053335f2694db8c05485558cde5176
carlosaguilar <carlos.aguilarhernandez.iesmiguelherrero.com>
2024-03-12 12:57:22 +0100

    modifico

--- sql/database.sql
+++ sql/database.sql
@@ -1,16 +1,19 @@
-CREATE TABLE users (
+CREATE TABLE comidas (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  surname VARCHAR(100) NOT NULL,
-  name VARCHAR(100) NOT NULL,
-  age INT UNSIGNED NOT NULL
+  nombre VARCHAR(100) NOT NULL,
+  tipocomida VARCHAR(100) NOT NULL,
+  cuandocomer VARCHAR(100) NOT NULL,
+  paiscreacion VARCHAR(100) NOT NULL,
+  paisauge VARCHAR(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO users (surname, name, age) VALUES('Casas', 'Julian', 27);
-INSERT INTO users (surname, name, age) VALUES('Oro', 'Carlos', 32);
-INSERT INTO users (surname, name, age) VALUES('Valencia', 'Pablo', 29);
-INSERT INTO users (surname, name, age) VALUES('Dimas', 'Maria', 19);
-INSERT INTO users (surname, name, age) VALUES('Corrales', 'Marcos', 26);
-INSERT INTO users (surname, name, age) VALUES('Perez', 'Juan', 21);
-INSERT INTO users (surname, name, age) VALUES('Zapata', 'Pedro', 23);
+INSERT INTO comidas (nombre, tipocomida, cuandocomer, paiscreacion, paisauge) VALUES('Croquetas', 'Salada', 'Entrante', 'Francia', 'España');
+INSERT INTO comidas (nombre, tipocomida, cuandocomer, paiscreacion, paisauge) VALUES('CocidoMontañes', 'Salada', 'PlatoPrincipal', 'España', 'España');
+INSERT INTO comidas (nombre, tipocomida, cuandocomer, paiscreacion, paisauge) VALUES('Anchoas', 'Salada', 'Entrante', 'Italia', 'España');
+INSERT INTO comidas (nombre, tipocomida, cuandocomer, paiscreacion, paisauge) VALUES('Sobaos', 'Dulce', 'Postre', 'España', 'España');
+INSERT INTO comidas (nombre, tipocomida, cuandocomer, paiscreacion, paisauge) VALUES('AlmejasMarinera', 'Salada', 'PlatoPrincipal', 'España', 'España');
+INSERT INTO comidas (nombre, tipocomida, cuandocomer, paiscreacion, paisauge) VALUES('TartaHojaldre', 'Dulce', 'Postre', 'Francia', 'España');
+INSERT INTO comidas (nombre, tipocomida, cuandocomer, paiscreacion, paisauge) VALUES('Rabas', 'Salada', 'Entrante', 'España', 'España');
+INSERT INTO comidas (nombre, tipocomida, cuandocomer, paiscreacion, paisauge) VALUES('Quesada', 'Dulce', 'Postre', 'España', 'España');
 
 
